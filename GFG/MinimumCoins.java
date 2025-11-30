b5f3292058300c37c2449c7600fec8ead961182d
komalpreetkaur18278@gmail.com <komalpreetkaur18278@gmail.com>
2025-11-30 19:12:36 +0530

    ques

---
+++ GFG/MinimumCoins.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findMin(int n) {
+        // code here
+        int totalCoins=0;
+        int coins[]={10,5,2,1};
+        for(int c:coins){
+            int count=n/c;
+            totalCoins+=count;
+            n-=(count*c);
+            if(n==0)break;
+        }
+        return totalCoins;
+    }
+}
